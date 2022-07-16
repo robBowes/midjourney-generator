bb448c04f8f8cc0e29b2346bedde76d8eeae1aec
Robert Bowes <r.e.bowes@gmail.com>
2022-07-16 11:16:39 -0400

    Add artist dropdown

--- components/MidjoyrneyForm.tsx
+++ components/MidjoyrneyForm.tsx
@@ -1,21 +1,76 @@
 import { useState } from "react"
 import { Field, Form } from "react-final-form"
+import TextField from "@mui/material/TextField"
 
-type MidjoyrneyInputs = { mainIdea: string }
+type MidjoyrneyInputs = {
+  mainIdea: string
+  byArtist: boolean
+  artistName: string
+  test: boolean
+}
 
 export const MidjourneyForm = () => {
   const [midjourneyPrompt, setMidjourneyPrompt] = useState("")
 
-  const onSubmit = ({ mainIdea }: MidjoyrneyInputs) => {
-    setMidjourneyPrompt(mainIdea)
+  const onSubmit = ({
+    mainIdea,
+    test,
+    artistName,
+    byArtist,
+  }: MidjoyrneyInputs) => {
+    console.log(test)
+    const flags = [test && "--test"].filter(Boolean).join("")
+    const parts = [artistName && `painting by ${artistName}`]
+      .filter(Boolean)
+      .join("::")
+    const propmt = `${mainIdea}::${parts} ${flags}`
+    setMidjourneyPrompt(propmt)
+    navigator.clipboard.writeText(propmt)
   }
+
   return (
     <div>
       <Form<MidjoyrneyInputs>
         onSubmit={onSubmit}
-        render={({ handleSubmit }) => (
+        initialValues={{ mainIdea: "" }}
+        render={({ handleSubmit, errors }) => (
           <form onSubmit={handleSubmit}>
-            <Field component='input' name='mainIdea' />
+            <div>
+              <label htmlFor='mainIdea'></label>
+              <Field<string>
+                name='mainIdea'
+                render={({ input }) => (
+                  <TextField
+                    label='Main Idea'
+                    variant='standard'
+                    id='mainIdea'
+                    value={input.value}
+                    onChange={input.onChange}
+                    required
+                  />
+                )}
+                validate={(value) => (!value ? "required" : undefined)}
+              />
+            </div>
+            <div>
+              <div>
+                <label htmlFor='byArtist'>By artist:</label>
+
+                <Field<string>
+                  component='select'
+                  name='artistName'
+                  id='mainIdea'
+                  validate={(value) => (!value ? "required" : undefined)}
+                >
+                  <option />
+                  <option value='Hiroshige'>Hiroshige</option>
+                </Field>
+              </div>
+            </div>
+            <div>
+              <label htmlFor='test'>Test</label>
+              <Field component='input' type='checkbox' name='test' id='test' />
+            </div>
             <button type='submit'>Submit</button>
           </form>
         )}
